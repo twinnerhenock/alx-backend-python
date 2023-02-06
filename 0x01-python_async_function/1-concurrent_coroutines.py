6def6a43cbc4042860b84719db7aef9d7339dcdc
twinnerhenock <demessiehe@gmail.com>
2023-02-06 12:22:28 +0000

    async using spawned function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-'''Task 1's module.
-'''
+""" The basics of async """
+
 import asyncio
 from typing import List
-
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
-
+    """
+    spawn wait_random n times with the specified max_delay.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
